fb949c943aff967dc3772fd6d4cf633f85fcb781
Maksim_Gladkov <60353057+Hokage-git@users.noreply.github.com>
2022-08-17 15:59:55 +0500

    Init
    Первая версия чата, работает отправка сообщения отдельному пользователю со стороны сервера, но пока пользователи не могут отправлять сообщения другим пользователям, только серверу

---
+++ Chat/Chat/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using SuperSimpleTcp;
+using System;
+using System.Windows.Threading;
+using System.Net;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Chat
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        SimpleTcpClient tcpClient;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var data = new byte[4];
+            new Random().NextBytes(data);
+            IPAddress ip = new IPAddress(data);
+            ipTxt.Text = "127.0.0.1:4747";//ip.ToString() + $":{new Random().Next(1000, 9999)}";
+            tcpClient = new SimpleTcpClient(ipTxt.Text);
+
+            tcpClient.Events.Connected += Events_Connected;
+            tcpClient.Events.Disconnected += Events_Disconnected;
+            tcpClient.Events.DataReceived += Events_DataReceived;
+        }
+
+        private void Events_DataReceived(object sender, DataReceivedEventArgs e)
+        {
+            Dispatcher.BeginInvoke(new Action(delegate
+            {
+                chatMesHistory.Items.Add($"{Encoding.UTF8.GetString(e.Data)}");
+            }));
+        }
+
+        private void Events_Disconnected(object sender, ConnectionEventArgs e)
+        {
+            Dispatcher.BeginInvoke(new Action(delegate
+            {
+                chatMesHistory.Items.Add($"Disconnected");
+            }));
+        }
+
+        private void Events_Connected(object sender, ConnectionEventArgs e)
+        {
+            Dispatcher.BeginInvoke(new Action(delegate
+            {
+                chatMesHistory.Items.Add($"Connected");
+            }));
+        }
+
+        private void nameConfirmBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try 
+            {
+                tcpClient.Connect();
+                nameConfirmBtn.IsEnabled = false;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Cервер не запущен","Message",MessageBoxButton.OK,MessageBoxImage.Error);
+            }
+        }
+
+        private void messageTxt_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key != Key.Enter) return;
+            else if (!string.IsNullOrEmpty(messageTxt.Text))
+            {
+                chatMesHistory.Items.Add($"{nameTxt.Text}: {messageTxt.Text}");
+                tcpClient.Send(messageTxt.Text);
+                messageTxt.Text = string.Empty;
+            }
+            e.Handled = true;
+        }
+    }
+}
---
+++ ChatServer/ChatServer/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using SuperSimpleTcp;
+using System;
+using System.Text;
+using System.Windows.Threading;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ChatServer
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        SimpleTcpServer tcpServer;
+
+        private void connectButton_Click(object sender, RoutedEventArgs e)
+        {
+            tcpServer.Start();
+            connectButton.IsEnabled = false;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            tcpServer = new SimpleTcpServer(ipTxt.Text);
+            tcpServer.Events.ClientConnected += Events_ClientConnected;
+            tcpServer.Events.ClientDisconnected += Events_ClientDisconnected;
+            tcpServer.Events.DataReceived += Events_DataReceived;
+        }
+
+        private void Events_DataReceived(object sender, DataReceivedEventArgs e)
+        {
+            Dispatcher.BeginInvoke(new Action(delegate
+            {
+                messageBlock.Items.Add($"{e.IpPort}: {Encoding.UTF8.GetString(e.Data)}");
+            }));
+        }
+
+        private void Events_ClientDisconnected(object sender, ConnectionEventArgs e)
+        {
+            Dispatcher.BeginInvoke(new Action(delegate
+            {
+                messageBlock.Items.Add($"{e.IpPort} has Disconnected");
+                userList.Items.Remove(e.IpPort);
+            }));
+        }
+
+        private void Events_ClientConnected(object sender, ConnectionEventArgs e)
+        {
+            Dispatcher.BeginInvoke(new Action(delegate
+            {
+                messageBlock.Items.Add($"{e.IpPort} Connected");
+                userList.Items.Add(e.IpPort);
+            }));
+        }
+
+        private void messageBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key != Key.Enter) return;
+            else if(!string.IsNullOrEmpty(messageBox.Text) && userList.SelectedItem != null)
+            {
+                messageBlock.Items.Add($"Server: {messageBox.Text}");
+                tcpServer.Send(userList.SelectedItem.ToString(), messageBox.Text);
+                messageBox.Text = string.Empty;
+            }
+            e.Handled = true;
+        }
+    }
+}
